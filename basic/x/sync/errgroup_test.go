1dc1c223217e0361dd6e142f5ccaaf9c887871b6
kwseekerli <kwseekerli@welove-inc.com>
2022-12-20 19:31:17 +0800

    x/sync包 errgroup

---
+++ basic/x/sync/errgroup_test.go
@@ -0,0 +1,94 @@
+package sync
+
+import (
+	"context"
+	"errors"
+	"testing"
+	"time"
+
+	"golang.org/x/sync/errgroup"
+)
+
+func TestZeroGroup(t *testing.T) {
+	err1 := errors.New("errgroup_test: 1")
+	err2 := errors.New("errgroup_test: 2")
+
+	cases := []struct {
+		errs []error
+	}{
+		{errs: []error{}},
+		{errs: []error{nil}},
+		{errs: []error{err1}},
+		{errs: []error{err1, nil}},
+		{errs: []error{err1, nil, err2}},
+	}
+
+	for _, tc := range cases {
+		g := new(errgroup.Group)
+
+		var firstErr error
+		for i, err := range tc.errs {
+			err := err
+			//在新的协程中执行func
+			g.Go(func() error {
+				time.Sleep(time.Second)
+				t.Log(err)
+				return err
+			})
+
+			if firstErr == nil && err != nil {
+				firstErr = err
+			}
+
+			//阻塞等待所有Go()协程执行完毕，返回第一个非nil的错误
+			gErr := g.Wait()
+			t.Log("continue")
+			if gErr != firstErr {
+				t.Errorf("after %T.Go(func() error { return err }) for err in %v\n"+
+					"g.Wait() = %v; want %v",
+					g, tc.errs[:i+1], err, firstErr)
+			}
+		}
+	}
+}
+
+func TestWithContext(t *testing.T) {
+	errDoom := errors.New("group_test: doomed")
+
+	cases := []struct {
+		errs []error
+		want error
+	}{
+		{want: nil},
+		{errs: []error{nil}, want: nil},
+		{errs: []error{errDoom}, want: errDoom},
+		{errs: []error{errDoom, nil}, want: errDoom},
+	}
+
+	for _, tc := range cases {
+		g, ctx := errgroup.WithContext(context.Background())
+
+		for _, err := range tc.errs {
+			err := err
+			g.Go(func() error { return err })
+		}
+
+		if err := g.Wait(); err != tc.want {
+			t.Errorf("after %T.Go(func() error { return err }) for err in %v\n"+
+				"g.Wait() = %v; want %v",
+				g, tc.errs, err, tc.want)
+		}
+
+		canceled := false
+		select {
+		case <-ctx.Done():
+			canceled = true
+		default:
+		}
+		if !canceled {
+			t.Errorf("after %T.Go(func() error { return err }) for err in %v\n"+
+				"ctx.Done() was not closed",
+				g, tc.errs)
+		}
+	}
+}
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.17
 require (
 	github.com/sirupsen/logrus v1.8.1
 	golang.org/x/net v0.0.0-20220930213112-107f3e3c3b0b
+	golang.org/x/sync v0.1.0
 	gorm.io/driver/mysql v1.3.6
 	gorm.io/gorm v1.23.8
 )
